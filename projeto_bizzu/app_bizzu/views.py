a6a13feec3db2f92092fa95ef36ff4038e0b3067
Ana Maria <144686482+namariaa@users.noreply.github.com>
2025-01-05 03:17:23 +0000

    criação do botão genérico

--- projeto_bizzu/app_bizzu/views.py
+++ projeto_bizzu/app_bizzu/views.py
@@ -4,5 +4,11 @@ from django.http import HttpResponse, HttpResponseRedirect
 # Create your views here.
 
 def index(request):
-    context = {'nome':'ana'}
-    return render(request,'naveBar1.html', context)
+    context = {
+        'cor':'#058B92',
+        'link': '#',
+        'icone': '/static/img/desconectar.svg',
+        'label': 'ver'
+        }
+    #  #058B92
+    return render(request,'feed.html', context)
