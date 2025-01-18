e10aec85007cd5db786cb4980bed77649c741807
Ana Maria <144686482+namariaa@users.noreply.github.com>
2025-01-18 04:35:05 +0000

    Finalização botão e input

--- projeto_bizzu/app_bizzu/static/js/dropdownBotao.js
+++
@@ -1,3 +0,0 @@
-function dropOrdenar(){
-    alert("heelo")
-}
--- projeto_bizzu/app_bizzu/views.py
+++ projeto_bizzu/app_bizzu/views.py
@@ -5,18 +5,6 @@ from django.http import HttpResponse, HttpResponseRedirect
 
 def index(request):
     context = {
-        'corBotao':'#9D0707',
-        'corFonteBotao': '#ffffff',
-        'linkBotao': '#',
-        'iconeBotao': '/static/img/icone pagina inicial.png ',
-        'labelBotao': 'Sair', 
-        #'strokeBotao': '#058B92',
         'placeInput' : 'senha',
-        #'corBotao2':'#9D0707',
-        'corFonteBotao2': '#F79010',
-        'linkBotao2': '#',
-        #'iconeBotao2': '/static/img/icone pagina inicial.png ', #apenas se for selecionar comunidade
-        'labelBotao2': 'Mais recente', 
-        'strokeBotao2': '#F79010',
         }
     return render(request,'feed.html', context)
