807d48f33678b1dd01f22bee654507f784a2958a
Ana Maria <144686482+namariaa@users.noreply.github.com>
2025-01-24 01:30:32 +0000

    [feat]:✨  Adiciona estrutura dos models

--- projeto_bizzu/app_bizzu/admin.py
+++ projeto_bizzu/app_bizzu/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Usuario, Postagem, Repositorio, Comentario, Comunidade, Categoria
 
-# Register your models here.
+admin.site.register(Usuario)
+admin.site.register(Postagem)
+admin.site.register(Repositorio)
+admin.site.register(Comentario)
+admin.site.register(Comunidade)
+admin.site.register(Categoria)
