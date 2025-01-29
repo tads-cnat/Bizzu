334107e5221a63b3af9058357c1696309baa5ebc
luiizr <luizroberto312005@gmail.com>
2025-01-29 17:26:04 -0300

    Bug fix

--- projeto_bizzu/app_bizzu/views.py
+++ projeto_bizzu/app_bizzu/views.py
@@ -9,6 +9,11 @@ from django.core.exceptions import ValidationError
 from .models import Postagem, Comentario, Repositorio, Comunidade, Usuario, Curtida
 from django.contrib.auth import logout
 from django.core.paginator import Paginator
+from django.contrib import messages
+from django.contrib.auth import login as login_django
+
+
+
 
 # @login_required(login_url="/login/")
 # def feed(request):
@@ -95,6 +100,7 @@ def cadastro(request):
         user = Usuario.objects.create_user(username=username, email=email, password=senha)
         # user.foto_perfil = foto_perfil
         user.save()
+        login_django(request, user)
         # return redirect('login')
         return redirect('escolher_comunidade')
 
@@ -169,15 +175,11 @@ def sair(request):
 #             return redirect('feed')
 #     return render(request, 'comunidade.html')
 
-
 @login_required
 def escolher_comunidade(request):
     comunidades = Comunidade.objects.all()  # Buscar todas as comunidades do banco
     return render(request, "comunidade.html", {"comunidades": comunidades})
 
-
-
-from django.contrib import messages
 
 @login_required
 def associar_comunidade(request):
