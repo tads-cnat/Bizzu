22249bbb18fe401f6bde538cb924a735aac13a0b
namariaa <anamariafag18@gmail.com>
2025-02-21 22:54:58 -0300

    Muda para postgres

--- projeto_bizzu/projeto_bizzu/settings.py
+++ projeto_bizzu/projeto_bizzu/settings.py
@@ -77,11 +77,14 @@ WSGI_APPLICATION = "projeto_bizzu.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "bizzuDb",
+        "PASSWORD": "bizzu",
+        "USER": "bizzu",
+        "PORT": 5432,
+        "HOST": "db", #Nome do service 
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
