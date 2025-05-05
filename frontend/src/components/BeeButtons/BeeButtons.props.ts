3c64c7b4b1be6982876cf7fe9cd8babe7488700f
Beto <164033637+luiizr@users.noreply.github.com>
2025-05-05 00:00:08 +0000

    feat: Implementa modo de criação de botão específico

--- frontend/src/components/BeeButtons/BeeButtons.props.ts
+++ frontend/src/components/BeeButtons/BeeButtons.props.ts
@@ -1,17 +1,24 @@
 // BeeButton.props.ts
 import { ReactNode } from 'react';
+// Aqui é onde ficam as variantes, então se você precisar criar um novo botão ou a variante de um botão existente, crie uma tag/nome para esse botão aqui
+export type BeeButtonVariantes = 'primaria' | 'secundaria' | 'negativo' | 'aviso' | 'neutro';
+// Aqui são os tamanhos pré-determinados dos botões, não está sendo usado pois da para aumentar ou diminuir usando tailwind, 
+// mas ele existe e está ai para se modificar caso precise
+export type BeeButtonTamanhos = 'pequeno' | 'medio' | 'grande';
 
-export type BeeButtonVariant = 'primary' | 'secondary' | 'danger' | 'warning' | 'neutral';
-export type BeeButtonSize = 'sm' | 'md' | 'lg';
+// Aqui é o props/Interface dos botões, onde ficam todas as coisas possíveis para se fazer com os botões, então caso você precise de algo
+// específico que não existe nos botões, adicione ele ao props (lembrando de adicionar uma interrogação ao final do atributo, para que ela não seja
+// obrigatória em todos os)
 
 export interface BeeButtonProps {
-  label: string;
-  icon?: ReactNode;
+  label?: string;
+  icone?: ReactNode;
   onClick?: () => void;
-  isLoading?: boolean;
-  variant?: BeeButtonVariant;
-  size?: BeeButtonSize;
+  estaCarregando?: boolean;
+  variante?: BeeButtonVariantes;
+  tamanho?: BeeButtonTamanhos;
   className?: string;
   href?: string;
-  disabled?: boolean;
+  desabilitado?: boolean;
+  classesDefault?: boolean;
 }
--- frontend/src/components/BeeButtons/BeeButtons.tsx
+++ frontend/src/components/BeeButtons/BeeButtons.tsx
@@ -1,31 +1,48 @@
 import { BeeButtonProps } from './BeeButtons.props';
 
+// Aqui estamos criando a base dos botões, todos eles seguem um padrão, e esse padrão é determinado aqui
+// Caso precise modificar o modelo do botão de forma específica, modificar aqui, criando uma constante e adicionando ao seu botão
+// a sua constante criada. Lembre-se de nunca mexer nas especificações bases do componente, pois pode gerar atritos no restante
+// no mais chamar Luiz Roberto para resolver isso
+
 export const BeeButton = ({
   label,
-  icon,
-  variant = 'primary',
+  icone,
+  variante = 'primaria',
   onClick,
-  disabled = false,
+  desabilitado = false,
+  classesDefault = true,
+  className= '',
 }: BeeButtonProps) => {
-  const baseClasses =
+  // Aqui estão as classes defaults em tailwind para os botões 
+  const classesPrimarias =
     'flex items-center justify-center gap-2 px-4 py-2 rounded-md font-medium text-sm transition-all duration-200';
-  const disabledClasses = 'opacity-50 cursor-not-allowed';
+  // Aqui está a classe para botões desabilitados
+    const classesDesabilitado = 'opacity-50 cursor-not-allowed';
+  const classeAtivo = 'cursor-pointer'
 
-  const variantClasses = {
-    primary: 'bg-teal-600 text-white hover:bg-teal-700',
-    secondary: 'bg-teal-500 text-white hover:bg-teal-600',
-    danger: 'bg-red-600 text-white hover:bg-red-700',
-    warning: 'bg-yellow-400 text-white hover:bg-yellow-500',
-    neutral: 'bg-gray-300 text-black hover:bg-gray-400',
+  // Aqui são as variáveis existentes, então ao criar uma nova, adicionar a cor do botão, a cor do texto e o hover dela aqui!
+  const classesVariantes = {
+    primaria: 'bg-teal-600 text-white hover:bg-teal-700',
+    secundaria: 'bg-teal-500 text-white hover:bg-teal-600',
+    negativo: 'bg-red-600 text-white hover:bg-red-700',
+    aviso: 'bg-yellow-400 text-white hover:bg-yellow-500',
+    neutro: 'bg-gray-300 text-black hover:bg-gray-400',
   };
 
-  const classes = `${baseClasses} ${variantClasses[variant]} ${
-    disabled ? disabledClasses : ''
-  }`;
+  // Aqui é onde vamos referenciar as classes que criamos para dentro do tailwind do botão principal, então na constante classes nós temos
+  // as variantes que criamos dentro das ClassesPrimarias e das Classes Variantes para ele adicionar tanto a cor quanto o modelo do botão
+  // e também temos o tailwind do botão desabilitado sendo aplicado caso o desenvolvedor escolha desabilitar o botão
 
-  return (
-    <button onClick={onClick} disabled={disabled} className={classes}>
-      {icon && <span>{icon}</span>}
+  const classes = classesDefault ? `${classesPrimarias} ${classesVariantes[variante]} ${
+        desabilitado ? classesDesabilitado : classeAtivo
+      }`
+    : className; // usa as classes personalizadas diretamente
+
+  return ( // Talvez o desabilitado esteja aparecendo com erro para algumas pessoas, mas ele está funcionando normalmente, não sei o porque 
+           // ele aparece como erro
+    <button onClick={onClick} desabilitado={desabilitado} className={classes} classesDefault={classesDefault}>
+      {icone && <span>{icone}</span>}
       <span>{label}</span>
     </button>
   );
