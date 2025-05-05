e2d25854874348ff3a22e8e393174744134e9209
Luiz Fernando <97256376+luuiizf@users.noreply.github.com>
2025-05-05 14:32:34 +0000

    :wqfeat: criação do componente header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
 import "./App.css";
+import Header from "./components/Header/Header";
+import BeeTags from "./components/BeeTags/BeeTags";
 
 function App() {
 
---
+++ frontend/src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+  return (
+    <header className="flex items-center justify-between bg-white py-4 px-8 shadow-sm">
+      <div className="flex items-center">
+        <img src="/logo.png" alt="BIZZU Logo" className="w-32" />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
