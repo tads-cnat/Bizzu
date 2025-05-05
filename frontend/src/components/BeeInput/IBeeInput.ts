0fe4a519c59e1ffdce8654ae5a5101b14002947f
Fabioasl <fabioalexandre07978@gmail.com>
2025-05-05 10:48:02 -0300

    feat: Cria componente input

---
+++ frontend/src/components/BeeInput/IBeeInput.ts
@@ -0,0 +1,9 @@
+export default interface IBeeInput {
+    label: string; // texto que vai ser exibido em cima do input
+    placeholder: string; // texto que vai ficar dentro do input
+    image?: HTMLImageElement; // imagens usadas no input
+    type?: string; // tipo de entrada do input
+}
+
+
+
