80057ee53f3ba605fde56332567956bd3a373c03
Fabioasl <fabioalexandre07978@gmail.com>
2025-05-05 21:09:57 -0300

    fix: conserta código do input e remove css

--- frontend/src/components/BeeInput/BeeInput.tsx
+++ frontend/src/components/BeeInput/BeeInput.tsx
@@ -2,19 +2,23 @@
 
 import {JSX} from "react";
 import IBeeInput from "./IBeeInput";
-import styles from './BeeInput.module.css'; // Caso queira mudar o tamanho do input basta ir nesse arquivo e em input mudar o height
+import { User } from "phosphor-react"; // caso queira mudar o ícone é so acessar https://phosphoricons.com/ e mudar o que vem após o import {"Exemplo"}
 
 export default function BeeInput(props: IBeeInput): JSX.Element{
-        const {label, placeholder,type} = props;
-        return (
-                <>
-                <div className={styles.inputcontainer}>
-                        <label  className={styles.label}>{label}</label> 
-                        <input type={type} className={styles.input} placeholder={placeholder} />
+    const {label, placeholder,type} = props;
+    return (
+        <>
+            <div>
+            <label htmlFor="price" className="block text-sm/6 font-medium text-gray-900">{label}</label>
+                <div className="mt-2">
+                    <div className="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-indigo-600">
+                        <div> <User size={20} color="#000" weight="bold" /></div>
+                        <input type={type} placeholder={placeholder} className="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"/>
+                    </div>
                 </div>
-                </>
+            </div>
 
-                
+        </>
         );
 
 }
