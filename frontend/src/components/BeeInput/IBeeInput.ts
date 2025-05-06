d2fe6f3e9dc2f64f36539a1dbeebcc51caf03e4a
Fábio Alexandre <163214715+Fabioasl@users.noreply.github.com>
2025-05-06 01:25:03 -0300

    fix: Altera props

--- frontend/src/components/BeeInput/IBeeInput.ts
+++ frontend/src/components/BeeInput/IBeeInput.ts
@@ -3,7 +3,6 @@ import { Icon } from "phosphor-react";
 export default interface IBeeInput {
     label: string; // texto que vai ser exibido em cima do input
     placeholder: string; // texto que vai ficar dentro do input
-    image?: HTMLImageElement; // imagens usadas no input
     type?: string; // tipo de entrada do input
     icon?: Icon; // Ícone caso necessário para usar no input
 }
