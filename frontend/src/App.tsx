dcfe760992679788be0775956fd3f82e96cb6036
Jesrriel Moura <jesrrielriri@gmail.com>
2025-05-06 11:53:36 -0300

    :wqfeat: Cria componente de barra de pesquisa

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+<<<<<<< HEAD
 import {
 	House,
 	Users,
@@ -6,11 +7,16 @@ import {
   } from '@phosphor-icons/react';
 
   import { BeeSidebar } from './components/BeeSidebar/BeeSidebar.tsx';
+=======
+import BeeTags from "./components/BeeTags/BeeTags";
+import BeeSearchBar from "./components/BeeSearchBar/BeeSearchBar";
+>>>>>>> 9277d80 (feat: Cria componente de barra de pesquisa)
 
 function App() {
 
 	return (
 		<>
+<<<<<<< HEAD
 <div className="bg-gray-800">
 	
 			  <BeeSidebar
@@ -57,3 +63,18 @@ function App() {
 }
 
 export default App;
+=======
+			<BeeTags
+				label="INFOWEB"
+				color="#FCBD18"
+			/>
+
+			<BeeSearchBar onSearch={(value) => console.log("Busca:", value)} />
+		</>
+	);
+}
+
+export default App;
+
+//Estou está página inicialmente para testes  altere para a maneira que deveria ser
+>>>>>>> 9277d80 (feat: Cria componente de barra de pesquisa)
--- frontend/src/components/BeeSearchBar/IBeeSearchBar.ts
+++ frontend/src/components/BeeSearchBar/IBeeSearchBar.ts
@@ -1,5 +1,8 @@
 export interface IBeeSearchBar {
+<<<<<<< HEAD
 	// Função chamada quando o usuário realiza uma busca.
 	//Recebe como parâmetro o termo digitado na barra de busca(string).
+=======
+>>>>>>> 9277d80 (feat: Cria componente de barra de pesquisa)
 	onSearch: (termo: string) => void;
 }
