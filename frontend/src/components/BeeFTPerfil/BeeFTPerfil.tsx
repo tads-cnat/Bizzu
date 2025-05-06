0184ab4fbe7e115591d443114f80ac46a4c230c0
Fábio Alexandre <163214715+Fabioasl@users.noreply.github.com>
2025-05-06 00:53:00 +0000

    feat: Adiciona componente Foto + Nome perfil

---
+++ frontend/src/components/BeeFTPerfil/BeeFTPerfil.tsx
@@ -0,0 +1,20 @@
+import {JSX} from "react";
+import IBeeFTPerfil from "./IBeeFTPerfil";
+import styles from './BeeFTPerfil.module.css';
+
+export default function BeeFTPerfil(props: IBeeFTPerfil): JSX.Element{
+        const {name,time,image} = props;
+        return (
+            <>
+            <div className={styles.profileContainer}>
+                <div className={styles.hexagon}>
+                </div>
+                <div className="user-info">
+                    <span className={styles.name}>{name}</span>
+                    <span className={styles.span}> • {time}h atrás </span>
+                </div>
+            </div>
+            </>
+        )
+
+}
---
+++ frontend/src/components/BeeFTPerfil/IBeeFTPerfil.ts
@@ -0,0 +1,5 @@
+export default interface IBeeFTPerfil{
+    name: string; // texto referente ao nome da pessoa
+    time: string; // texto referente as horas que a pessoa fez a postagem
+    image?: File; // imagem que a pessoa coloca 
+}
