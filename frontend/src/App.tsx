9277d8014f2f43bfe071506db9ba6005b5bb16e2
Jesrriel Moura <jesrrielriri@gmail.com>
2025-05-06 11:53:36 -0300

    feat: Cria componente de barra de pesquisa

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import BeeTags from "./components/BeeTags/BeeTags";
+import BeeSearchBar from "./components/BeeSearchBar/BeeSearchBar";
 
 function App() {
 	return (
@@ -8,10 +9,12 @@ function App() {
 				label="INFOWEB"
 				color="#FCBD18"
 			/>
+
+			<BeeSearchBar onSearch={(value) => console.log("Busca:", value)} />
 		</>
 	);
 }
 
 export default App;
 
-//Estou está página inicialmente para testes
+//Estou está página inicialmente para testes  altere para a maneira que deveria ser
