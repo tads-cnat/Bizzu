1c1d7451a72f14dcce3852c130ccdc3621bb601b
Jesrriel Moura <jesrrielriri@gmail.com>
2025-05-06 23:15:48 -0300

    :wqfix: Alterações solicitadas

--- frontend/src/components/BeeSearchBar/IBeeSearchBar.ts
+++ frontend/src/components/BeeSearchBar/IBeeSearchBar.ts
@@ -1,8 +1,7 @@
 export interface IBeeSearchBar {
-<<<<<<< HEAD
 	// Função chamada quando o usuário realiza uma busca.
 	//Recebe como parâmetro o termo digitado na barra de busca(string).
-=======
->>>>>>> 9277d80 (feat: Cria componente de barra de pesquisa)
+	// Função chamada quando o usuário realiza uma busca.
+	//Recebe como parâmetro o termo digitado na barra de busca(string).
 	onSearch: (termo: string) => void;
 }
