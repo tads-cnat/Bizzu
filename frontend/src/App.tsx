76fcff8901841690e314487e4c8e67975ff526b3
Fabioasl <fabioalexandre07978@gmail.com>
2025-05-06 22:56:24 -0300

    fix: Refatora código de input

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,16 @@
+import {DiscordLogo, GithubLogo} from "@phosphor-icons/react";
 import "./App.css";
+import BeeInput from "./components/BeeInput/BeeInput";
 
 function App() {
-
 	return (
 		<>
-    </>
+			<BeeInput
+				label="usuario"
+				placeholder="placeholder"
+				icon={GithubLogo}
+			/>
+		</>
 	);
 }
 
