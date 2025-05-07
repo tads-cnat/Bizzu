6fb9bdbb37b16b28760c3792d0487a0274707eda
Beto <164033637+luiizr@users.noreply.github.com>
2025-05-07 17:26:33 +0000

    :wqFix: Ajuste rebase

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,9 @@
 import "./App.css";
-import BeeTags from "./components/BeeTags/BeeTags";
-import BeeSearchBar from "./components/BeeSearchBar/BeeSearchBar";
 
 function App() {
 
 	return (
 		<>
-			<BeeTags
-				label="INFOWEB"
-				color="#FCBD18"
-			/>
-
-			<BeeSearchBar onSearch={(value) => console.log("Busca:", value)} />
 		</>
 	);
 }
