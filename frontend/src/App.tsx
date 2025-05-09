29039c452dbb17a939599df0672cc6ec4cab8950
Jesrriel Moura <jesrrielriri@gmail.com>
2025-05-09 00:44:45 -0300

    feat: cria componente texte area

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,18 @@
 import "./App.css";
+import { BeeTextArea } from "./components/BeeTextArea/BeeTextArea";  // Remova a barra no final
 
 function App() {
-
-	return (
-		<>
-		</>
-	);
+  return (
+    <div className="p-4 max-w-xl mx-auto">
+      <BeeTextArea
+        id="descricao"
+        label="Descrição"
+        placeholder="Fale mais um pouco sobre você..."
+        defaultValue=""
+        onChange={(e) => console.log(e.target.value)}
+      />
+    </div>
+  );
 }
 
 export default App;
