471d96f7f23183a829220dcdba967351877b45fe
Jesrriel Moura <jesrrielriri@gmail.com>
2025-05-11 03:10:48 +0000

    Fix: Faz alterações solicitadas

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,11 @@
 import "./App.css";
-import { BeeTextArea } from "./components/BeeTextArea/BeeTextArea";  // Remova a barra no final
 
 function App() {
-  return (
-    <div className="p-4 max-w-xl mx-auto">
-      <BeeTextArea
-        id="descricao"
-        label="Descrição"
-        placeholder="Fale mais um pouco sobre você..."
-        defaultValue=""
-        onChange={(e) => console.log(e.target.value)}
-      />
-    </div>
-  );
+
+	return (
+		<>
+		</>
+	);
 }
 
 export default App;
