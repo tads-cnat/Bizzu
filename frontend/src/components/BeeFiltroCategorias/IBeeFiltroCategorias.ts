5aa342c5e89effc2895579da1f6bd8b328305a1b
Luiz Fernando <97256376+luuiizf@users.noreply.github.com>
2025-05-11 19:05:40 +0000

    feat: implementação do componente de filtro/categoria

---
+++ frontend/src/components/BeeFiltroCategorias/IBeeFiltroCategorias.ts
@@ -0,0 +1,13 @@
+// IBeeFiltroCategorias.ts
+export interface ICategoria {
+  id: number;
+  nome: string;
+  tipo: "tec" | "mat" | "per";
+}
+
+export interface IBeeFiltroCategorias {
+  categorias: ICategoria[];
+  categoriasSelecionadas: number[];
+  aoSelecionarCategoria: (id: number) => void;
+  aoPesquisar: (termo: string) => void;
+}
