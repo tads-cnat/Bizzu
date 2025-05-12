9a61118e682df7a4039fc5a3a401436fd7b5e897
Beto <164033637+luiizr@users.noreply.github.com>
2025-05-12 16:54:12 +0000

    Resolvendo conflitos

--- frontend/src/components/BeeSearchBar/IBeeSearchBar.ts
+++ frontend/src/components/BeeSearchBar/IBeeSearchBar.ts
@@ -1,10 +1,3 @@
 export interface IBeeSearchBar {
-<<<<<<< HEAD
-	// Função chamada quando o usuário realiza uma busca.
-	//Recebe como parâmetro o termo digitado na barra de busca(string).
-	// Função chamada quando o usuário realiza uma busca.
-	//Recebe como parâmetro o termo digitado na barra de busca(string).
-=======
->>>>>>> cebe68a (:wq:wqfeat: Cria componente de barra de pesquisa)
 	onSearch: (termo: string) => void;
 }
