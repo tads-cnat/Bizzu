bf07536d1178f149c28a7d5549fc347972e60e09
namariaa <anamariafag18@gmail.com>
2025-07-06 18:50:08 -0300

    remove: Retira imagens e arquivos desnecessários

--- frontend/src/utils/setRoleLocalStorage.ts
+++
@@ -1,7 +0,0 @@
-import { IBeeUsuario } from "../components/BeeFTPerfil/IBeeUsuario"
-
-const setRoleLocalStorage = (role:IBeeUsuario | null ) => {
-    localStorage.setItem("role", JSON.stringify(role));
-};
-
-export default setRoleLocalStorage;
