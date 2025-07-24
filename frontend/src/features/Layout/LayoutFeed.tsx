7ee82bf693b3c9ab1133c05152293e147f52079f
namariaa <anamariafag18@gmail.com>
2025-07-23 23:05:08 -0300

    fix: Modifia exibição do menu no feed

--- frontend/src/features/Layout/LayoutFeed.tsx
+++ frontend/src/features/Layout/LayoutFeed.tsx
@@ -315,7 +315,7 @@ const LayoutFeed = () => {
 			<BeeHeader />
 			<div className="flex flex-col flex-1 items-start w-1/5 mt-20">
 				<BeeSidebar onSelecionarSecao={handleSelecionarSecao} />
-				<div className="fixed top-[80px] ml-70 w-[66%] h-[calc(100vh-80px)] flex-1 flex flex-col px-3 py-4 rounded-xl z-40 overflow-y-auto justify-start items-center">
+				<div className="fixed top-[70px] ml-70 w-[66%] h-[calc(100vh-80px)] flex-1 flex flex-col px-3 py-4 rounded-xl z-40 overflow-y-auto justify-start items-center">
 					<div className="w-[550px] px-4 flex flex-col">
 						<div className="mb-4">
 							{filtrosAvancados && (
@@ -428,7 +428,7 @@ const LayoutFeed = () => {
 						</div>
 					</div>
 				</div>
-				<aside className="fixed top-[80px] right-4 w-[22%] min-h-screen shadow-md flex flex-col justify-start px-3 py-4 rounded-xl bg-white z-40 overflow-y-auto gap-4">
+				<aside className="fixed top-[70px] right-4 w-[22%] min-h-screen flex flex-col justify-start px-3 py-4 bg-white z-40 overflow-y-auto gap-4 border-l border-gray-300">
 					<h2 className="text-lg font-bold mb-2">Repositórios</h2>
 					{repositorios.length === 0 && (
 						<Empty
