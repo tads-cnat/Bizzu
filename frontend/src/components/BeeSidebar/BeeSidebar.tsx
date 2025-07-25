40bf07cfd631ee68ee4f9b22caf97c42aa513eaa
namariaa <anamariafag18@gmail.com>
2025-07-24 23:08:18 -0300

    fix: COnserta lógica de exibição de usuário sidebar

--- frontend/src/components/BeeSidebar/BeeSidebar.tsx
+++ frontend/src/components/BeeSidebar/BeeSidebar.tsx
@@ -14,7 +14,7 @@ import getLocalStorage from "../../utils/getLocalStorage";
 import BeeButton from "../BeeButtons/BeeButtons";
 
 export const BeeSidebar = ({onSelecionarSecao}: IBeeSidebarProps) => {
-	const username = getLocalStorage().username;
+	const [username, setUsername] = useState();
 	const [usuario, setUsuario] = useState<IBeeUser>();
 	const [comunidades, setComunidades] = useState<MenuItem[]>([]);
 	const [collapsed, setCollapsed] = useState(false);
@@ -22,6 +22,12 @@ export const BeeSidebar = ({onSelecionarSecao}: IBeeSidebarProps) => {
 	const toggleCollapsed = () => {
 		setCollapsed(!collapsed);
 	};
+
+	useEffect(() => {
+		if (getLocalStorage() != null && username == undefined) {
+			setUsername(getLocalStorage().username);
+		}
+	}, []);
 
 	useEffect(() => {
 		void UsuarioService.getbyUsername(username)
@@ -83,12 +89,12 @@ export const BeeSidebar = ({onSelecionarSecao}: IBeeSidebarProps) => {
 
 	return (
 		<>
-			{usuario == undefined ? (
+			{usuario == undefined && getLocalStorage() != null ? (
 				<Spin />
 			) : (
 				<div
-					className={`h-screen w-full ${
-						!collapsed ? "border-r border-gray-300" : ""
+					className={`h-screen transition-all duration-300 ${
+						collapsed ? "w-[80px]" : "w-[300px] border-r border-gray-300"
 					}`}
 				>
 					<div className="flex flex-col mt-4 ml-5 gap-3">
@@ -99,48 +105,50 @@ export const BeeSidebar = ({onSelecionarSecao}: IBeeSidebarProps) => {
 							className="w-fit"
 							classesDefault={false}
 						/>
-						{usuario && username ? (
-							<div className="flex items-center gap-3">
-								{!collapsed && (
-									<div className="flex items-center gap-3">
-										<img
-											src={
-												usuario.imagemPerfil
-													? `http://localhost:8000${usuario.imagemPerfil}`
-													: "http://localhost:8000/imgPostagens/usuarios/2025/06/10/sem_imagem_avatar.png"
-											}
-											alt={username}
-											className="w-10 h-10 object-cover"
-											style={{
-												clipPath:
-													"polygon(25% 6.7%, 75% 6.7%, 100% 50%, 75% 93.3%, 25% 93.3%, 0% 50%)",
-											}}
-										/>
-										<div className="leading-tight">
-											<p className="font-semibold text-md text-black">
-												{username}
-											</p>
-											<p className="text-xs text-zinc-500">
-												<Link to={`/${username}/`}>Ver perfil</Link>
-											</p>
+						<div className="flex items-center gap-3">
+							{!collapsed && (
+								<div>
+									{usuario && username ? (
+										<div className="flex items-center gap-3">
+											<img
+												src={
+													usuario.imagemPerfil
+														? `http://localhost:8000${usuario.imagemPerfil}`
+														: "http://localhost:8000/imgPostagens/usuarios/2025/06/10/sem_imagem_avatar.png"
+												}
+												alt={username}
+												className="w-10 h-10 object-cover"
+												style={{
+													clipPath:
+														"polygon(25% 6.7%, 75% 6.7%, 100% 50%, 75% 93.3%, 25% 93.3%, 0% 50%)",
+												}}
+											/>
+											<div className="leading-tight">
+												<p className="font-semibold text-md text-black">
+													{username}
+												</p>
+												<p className="text-xs text-zinc-500">
+													<Link to={`/${username}/`}>Ver perfil</Link>
+												</p>
+											</div>
 										</div>
-									</div>
-								)}
-							</div>
-						) : (
-							<div className="flex items-center">
-								<div className="relative px-4 py-2 bg-white border border-yellow-400 text-sm text-black font-medium rounded-[20px] shadow-md max-w-[200px] before:content-[''] before:absolute before:left-6 before:top-[-10px] before:w-0 before:h-0 before:border-l-[10px] before:border-l-transparent before:border-r-[10px] before:border-r-transparent before:border-b-[12px] before:border-b-white after:content-[''] after:absolute after:left-[25px] after:top-[-12px] after:w-0 after:h-0 after:border-l-[12px] after:border-l-transparent after:border-r-[12px] after:border-r-transparent after:border-b-[14px] after:border-b-yellow-400">
-									Ei!{" "}
-									<Link
-										to="/login/"
-										className="underline hover:text-yellow transition font-semibold"
-									>
-										Faça login
-									</Link>{" "}
-									pra se juntar a comunidade do bizzu!
+									) : (
+										<div className="flex items-center">
+											<div className="relative px-4 py-2 bg-white border border-yellow-400 text-sm text-black font-medium rounded-[20px] shadow-md max-w-[200px] before:content-[''] before:absolute before:left-6 before:top-[-10px] before:w-0 before:h-0 before:border-l-[10px] before:border-l-transparent before:border-r-[10px] before:border-r-transparent before:border-b-[12px] before:border-b-white after:content-[''] after:absolute after:left-[25px] after:top-[-12px] after:w-0 after:h-0 after:border-l-[12px] after:border-l-transparent after:border-r-[12px] after:border-r-transparent after:border-b-[14px] after:border-b-yellow-400">
+												Ei!{" "}
+												<Link
+													to="/login/"
+													className="underline hover:text-yellow transition font-semibold"
+												>
+													Faça login
+												</Link>{" "}
+												pra se juntar a comunidade do bizzu!
+											</div>
+										</div>
+									)}
 								</div>
-							</div>
-						)}
+							)}
+						</div>
 					</div>
 					{!collapsed && <Divider className="mt-400" />}
 					{username === undefined ? (
@@ -154,8 +162,10 @@ export const BeeSidebar = ({onSelecionarSecao}: IBeeSidebarProps) => {
 									defaultSelectedKeys={["1"]}
 									defaultOpenKeys={["3"]}
 									mode="inline"
+									inlineCollapsed={collapsed}
 									items={items}
 									selectable={false}
+									style={{width: "100%"}}
 								/>
 							}
 						/>
