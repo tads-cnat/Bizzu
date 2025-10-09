587e219a141b92233c8746b6336ee7e0888d9baa
namariaa <anamariafag18@gmail.com>
2025-10-09 16:55:45 -0300

    refactor: Insere controller no input.

--- frontend/src/components/BeeInput/IBeeInput.ts
+++ frontend/src/components/BeeInput/IBeeInput.ts
@@ -1,18 +1,19 @@
-import { IconProps } from "@phosphor-icons/react";
-import { UseFormRegisterReturn } from "react-hook-form";
- 
- 
- export  interface IBeeInput {
-     // texto que vai ser exibido em cima do input
-     label: string;
-     // texto que vai ficar dentro do input
-     placeholder: string; 
-     // tipo de entrada do input
-     type?: string; 
-     // Ícone caso necessário para usar no input
-     icon?: React.ElementType<IconProps>;
-    // Para registrar formulário
-    register?: UseFormRegisterReturn;
-    //Valor default
-    defaultValue?:string;
- }
+import {IconProps} from "@phosphor-icons/react";
+import {UseFormRegisterReturn} from "react-hook-form";
+
+export interface IBeeInput {
+	// texto que vai ser exibido em cima do input
+	label: string;
+	// texto que vai ficar dentro do input
+	placeholder: string;
+	// tipo de entrada do input
+	type?: string;
+	// Ícone caso necessário para usar no input
+	icon?: React.ElementType<IconProps>;
+	// Para registrar formulário
+	register?: UseFormRegisterReturn;
+	//Valor default
+	defaultValue?: string;
+	//Control do react form
+	control: any;
+}
