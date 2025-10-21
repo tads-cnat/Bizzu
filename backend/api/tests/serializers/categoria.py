1d664dd4233dc510b99234fe9d220190829099b9
Luiz Fernando <97256376+luuiizf@users.noreply.github.com>
2025-10-21 18:17:32 +0000

    feat: separa testes por modelo

--- backend/api/tests/serializers/categoria.py
+++ backend/api/tests/serializers/categoria.py
@@ -1,39 +1,6 @@
 from django.test import TestCase
 from django.core.exceptions import ValidationError
-from ..models import Categoria
 from ..serializers import CategoriaSerializer
-
-
-class CategoriaModelTest(TestCase):
-    """Testes unitários para o modelo Categoria"""
-
-    def setUp(self):
-        self.categoria = Categoria.objects.create(
-            nome="Tecnologia da Informação",
-            tipo="tec"
-        )
-
-    def test_criacao_com_sucesso(self):
-        """Deve criar uma categoria corretamente"""
-        self.assertEqual(self.categoria.nome, "Tecnologia da Informação")
-        self.assertEqual(self.categoria.tipo, "tec")
-
-    def test_str_retorna_nome(self):
-        """O método __str__ deve retornar o nome"""
-        self.assertEqual(str(self.categoria), "Tecnologia da Informação")
-
-    def test_tipo_invalido_dispara_erro(self):
-        """Deve gerar erro se tipo for inválido (fora das choices)"""
-        categoria_invalida = Categoria(nome="Teste Inválido", tipo="xyz")
-        with self.assertRaises(ValidationError):
-            categoria_invalida.full_clean()
-
-    def test_nome_vazio_dispara_erro(self):
-        """Não deve permitir nome vazio"""
-        categoria_vazia = Categoria(nome="", tipo="tec")
-        with self.assertRaises(ValidationError):
-            categoria_vazia.full_clean()
-
 
 class CategoriaSerializerTest(TestCase):
     """Testes unitários para o serializer CategoriaSerializer"""
--- backend/api/tests/serializers/curtida.py
+++ backend/api/tests/serializers/curtida.py
@@ -1,32 +1,8 @@
+from ..serializers import CurtidaSerializer
 from django.test import TestCase
 from ..models import Curtida
-from ..serializers import CurtidaSerializer
 from ..models.usuario import Usuario
 from ..models.postagem import Postagem
-
-
-class CurtidaModelTest(TestCase):
-    """Testes unitários para o modelo Curtida"""
-
-    def setUp(self):
-        self.usuario = Usuario.objects.create(username="luiz", password="123")
-        self.postagem = Postagem.objects.create(
-            titulo="Primeiro post", conteudo="Texto da postagem", autor=self.usuario
-        )
-        self.curtida = Curtida.objects.create(
-            usuario=self.usuario, postagem=self.postagem
-        )
-
-    def test_criacao_com_sucesso(self):
-        """Deve criar uma curtida com sucesso"""
-        self.assertEqual(self.curtida.usuario.username, "luiz")
-        self.assertEqual(self.curtida.postagem.titulo, "Primeiro post")
-
-    def test_str_retorna_texto_correto(self):
-        """O método __str__ deve exibir a frase esperada"""
-        esperado = f"{self.usuario} curtiu {self.postagem}"
-        self.assertEqual(str(self.curtida), esperado)
-
 
 class CurtidaSerializerTest(TestCase):
     """Testes unitários para o serializer CurtidaSerializer"""
