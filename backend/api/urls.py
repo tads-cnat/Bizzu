e890a9628b1e910ae587fe0c81fec12efd120a0c
Ana Maria <144686482+namariaa@users.noreply.github.com>
2026-05-18 18:01:25 +0000

    feat: Adiciona erro proposital para black. #339

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from .urls.postagem import postagemRouter
 from .urls.comunidade import comunidadeRouter
 from .urls.categoria import categoriaRouter
-    from .urls.curtida import curtidaRouter
+from .urls.curtida import curtidaRouter
 from .urls.repositorio import repositorioRouter
 from .urls.usuario import usuarioRouter
 from .urls.comentario import comentarioRouter
@@ -11,10 +11,10 @@ from .urls.arquivo import arquivoRouter
 urlpatterns = [
     path("", include(postagemRouter.urls)),
     path("", include(comunidadeRouter.urls)),
-                                    path("", include(categoriaRouter.urls)),
+    path("", include(categoriaRouter.urls)),
     path("", include(curtidaRouter.urls)),
     path("", include(repositorioRouter.urls)),
     path("", include(usuarioRouter.urls)),
-            path("", include(comentarioRouter.urls)),
+    path("", include(comentarioRouter.urls)),
     path("", include(arquivoRouter.urls)),
-]
+    ]
