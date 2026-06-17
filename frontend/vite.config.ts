59bb320413313abbfc7f9949936523849c09ac0c
Fábio Lucena <163214715+Fabioasl@users.noreply.github.com>
2026-06-17 17:09:58 +0000

    feat: Adiciona certificado autoassinado para rodar com https

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import tailwindcss from '@tailwindcss/vite'
+import fs from "fs";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    watch: {
-      usePolling: true,
-      interval: 1000
-    }
-  }
-})
+    https: {
+      key: fs.readFileSync("./certs/key.pem"),
+      cert: fs.readFileSync("./certs/cert.pem"),
+    },
+    host: "localhost",
+    port: 5173,
+  },
+});
